505a4405b121382476f8b6a8f1377f2d3e2467a6
sugahara <sugahara@weddingpark.co.jp>
2018-12-02 16:01:45 +0900

    fixed modal js

--- bundle.js
+++ bundle.js
@@ -95,10 +95,16 @@
 
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
-$(function(){
-    $('#modal').on('click', function () {
-        console.log(123);
-        $(this).css('display', 'block');
+$(function() {
+    $('#modal-button').on('click', function () {
+        var $modal = $('#modal');
+        if ($modal.is(':visible')) {
+            $(this).text('モーダル表示');
+            $modal.css('display', 'none');
+        } else {
+            $(this).text('モーダル非表示');
+            $modal.css('display', 'block');
+        }
     });
 });
 
